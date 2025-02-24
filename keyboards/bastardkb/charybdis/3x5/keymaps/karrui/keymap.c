30eb024d6642583900d6ae09c20e9ac480ab52e9
Kar Rui <hello@karrui.dev>
2025-02-24 23:36:11 +0800

    feat: add homerowmods process_record_user

--- keyboards/bastardkb/charybdis/3x5/keymaps/karrui/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/karrui/keymap.c
@@ -255,3 +255,64 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // rgb_matrix.c.
 void rgb_matrix_update_pwm_buffers(void);
 #endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+
+    case RCTL_T(KC_N):
+        /*
+        This piece of code nullifies the effect of Right Shift when tapping
+        the RCTL_T(KC_N) key.
+        This helps rolling over RSFT_T(KC_E) and RCTL_T(KC_N)
+        to obtain the intended "en" instead of "N".
+        Consequently, capital N can only be obtained by tapping RCTL_T(KC_N)
+        and holding LSFT_T(KC_S) (which is the left Shift mod tap).
+        */
+
+        /*
+        Detect the tap.
+        We're only interested in overriding the tap behavior
+        in a certain cicumstance. The hold behavior can stay the same.
+        */
+        if (record->event.pressed && record->tap.count > 0) {
+            // Detect right Shift
+            if (get_mods() & MOD_BIT(KC_RSHIFT)) {
+                // temporarily disable right Shift
+                // so that we can send KC_E and KC_N
+                // without Shift on.
+                unregister_mods(MOD_BIT(KC_RSHIFT));
+                tap_code(KC_E);
+                tap_code(KC_N);
+                // restore the mod state
+                add_mods(MOD_BIT(KC_RSHIFT));
+                // to prevent QMK from processing RCTL_T(KC_N) as usual in our special case
+                return false;
+            }
+        }
+         /*else process RCTL_T(KC_N) as usual.*/
+        return true;
+
+    case LCTL_T(KC_T):
+        /*
+        This piece of code nullifies the effect of Left Shift when
+        tapping the LCTL_T(KC_T) key.
+        This helps rolling over LSFT_T(KC_S) and LCTL_T(KC_T)
+        to obtain the intended "st" instead of "T".
+        Consequently, capital T can only be obtained by tapping LCTL_T(KC_T)
+        and holding RSFT_T(KC_E) (which is the right Shift mod tap).
+        */
+
+        if (record->event.pressed && record->tap.count > 0) {
+            if (get_mods() & MOD_BIT(KC_LSHIFT)) {
+                unregister_mods(MOD_BIT(KC_LSHIFT));
+                tap_code(KC_S);
+                tap_code(KC_T);
+                add_mods(MOD_BIT(KC_LSHIFT));
+                return false;
+            }
+        }
+         /*else process LCTL_T(KC_T) as usual.*/
+        return true;
+    }
+    return true;
+};
